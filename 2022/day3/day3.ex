2f030977e35756c55459f10e40ce9fb6ea98f80f
gidskql6671 <gidskql6671@gmail.com>
2022-12-07 16:40:43 +0900

    2022 Day3 - Part2 코드 작성

--- 2022/day3/day3.ex
+++ 2022/day3/day3.ex
@@ -1,14 +1,23 @@
 defmodule Day3 do
   def part1(input) do
     input
-    |> parse_input()
+    |> parse_input(1)
     |> find_dup_items()
     |> Enum.map(&calculate_type_score/1)
     |> Enum.sum()
     |> IO.inspect(label: "part1")
   end
 
-  defp parse_input(input) do
+  def part2(input) do
+    input
+    |> parse_input(2)
+    |> find_all_badge()
+    |> Enum.map(&calculate_type_score/1)
+    |> Enum.sum()
+    |> IO.inspect(label: "part2")
+  end
+
+  defp parse_input(input, 1) do
     input
     |> String.split("\n")
     |> Enum.map(fn line ->
@@ -16,6 +25,13 @@ defmodule Day3 do
 
       {String.graphemes(first), String.graphemes(second)}
     end)
+  end
+
+  defp parse_input(input, 2) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.chunk_every(3)
   end
 
   defp find_dup_items(rucksacks) do
@@ -31,6 +47,18 @@ defmodule Day3 do
     hd(all_types_compartments -- Enum.uniq(all_types_compartments))
   end
 
+  defp find_all_badge(groups) do
+    groups
+    |> Enum.map(&find_badge/1)
+  end
+
+  defp find_badge([first, second, third] = _group) do
+    (Enum.uniq(first) ++ Enum.uniq(second) ++ Enum.uniq(third))
+    |> Enum.reduce(%{}, fn item, acc -> Map.update(acc, item, 1, &(&1 + 1)) end)
+    |> Enum.find(fn {_k, count} -> count == 3 end)
+    |> elem(0)
+  end
+
   defp calculate_type_score(type) do
     ascii = :binary.first(type)
 
@@ -43,3 +71,4 @@ end
 
 File.read!("input.txt")
 |> tap(&Day3.part1/1)
+|> tap(&Day3.part2/1)
