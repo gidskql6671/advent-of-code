a314e3b3e5212b9b48fe7f2fd337e8a1513b2af2
gidskql6671 <gidskql6671@gmail.com>
2022-12-08 16:31:20 +0900

    2022 Day4 - Part1 코드 작성

---
+++ 2022/day5/day5.ex
@@ -0,0 +1,82 @@
+defmodule Day5 do
+  def part1(input) do
+    input
+    |> parse_input()
+    |> do_procedure()
+    |> print_top_crates()
+    |> IO.inspect(label: "part1")
+  end
+
+  defp parse_input(input) do
+    {started_stacks, procedure} =
+      input
+      |> String.split("\n")
+      |> Enum.split_while(& &1 != "")
+
+    {
+      parse_started_stacks(started_stacks),
+      parse_procedure(Enum.drop(procedure, 1))
+    }
+  end
+
+  defp parse_started_stacks(started_stacks) do
+    started_stacks
+    |> Enum.reverse()
+    |> Enum.drop(1)
+    |> Enum.reduce(%{}, fn line, result ->
+      line
+      |> String.graphemes()
+      |> Enum.chunk_every(4)
+      |> Enum.with_index(1)
+      |> Enum.reduce(result, fn
+        {["[", crate | _], index}, acc ->
+          Map.update(acc, index, [crate], & [crate | &1])
+
+        _, acc ->
+          acc
+      end)
+    end)
+  end
+
+  defp parse_procedure(procedure) do
+    regex = ~r/^move (?<count>[[:digit:]]+) from (?<from>[[:digit:]]+) to (?<to>[[:digit:]]+)$/
+
+    procedure
+    |> Enum.map(fn line ->
+      %{"count" => count, "from" => from, "to" => to} = Regex.named_captures(regex, line)
+
+      %{
+        count: String.to_integer(count),
+        from: String.to_integer(from),
+        to: String.to_integer(to),
+      }
+    end)
+  end
+
+  defp do_procedure({stacks, []}), do: stacks
+
+  defp do_procedure({stacks, procedure}) do
+    [%{count: count, from: from, to: to} | procedure] = procedure
+
+    stacks =
+      1..count
+      |> Enum.reduce(stacks, fn _, acc ->
+        [from_top | from_tail] = Map.get(acc, from)
+
+        acc
+        |> Map.update!(to, &[from_top | &1])
+        |> Map.put(from, from_tail)
+      end)
+
+    do_procedure({stacks, procedure})
+  end
+
+  defp print_top_crates(stacks) do
+    stacks
+    |> Enum.map(fn {_index, [top | _tail]} -> top end)
+    |> Enum.join()
+  end
+end
+
+File.read!("input.txt")
+|> tap(&Day5.part1/1)
