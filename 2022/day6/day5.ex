15d8c2f305abc4817c975b202aa4f3e16b9ae5f3
gidskql6671 <gidskql6671@gmail.com>
2022-12-09 17:22:44 +0900

    2022 Day6 - Part2 코드 작성

--- 2022/day6/day5.ex
+++ 2022/day6/day5.ex
@@ -1,22 +1,28 @@
 defmodule Day6 do
   def part1(input) do
     input
-    |> find_first_start_of_packet_position()
+    |> find_distinct_characters_position(4)
     |> IO.inspect(label: "part1")
   end
 
-  defp find_first_start_of_packet_position(datastream) do
+  def part2(input) do
+    input
+    |> find_distinct_characters_position(14)
+    |> IO.inspect(label: "part1")
+  end
+
+  defp find_distinct_characters_position(datastream, count) do
     datastream
     |> String.graphemes()
     |> Enum.with_index()
     |> Enum.reduce_while([], fn {ele, index}, acc ->
       cond do
-        index > 3 ->
+        index > count - 1 ->
           [_ | acc] = acc ++ [ele]
 
           if dif_all?(acc), do: {:halt, index + 1}, else: {:cont, acc}
 
-        index == 3 ->
+        index == count - 1 ->
           acc = acc ++ [ele]
 
           if dif_all?(acc), do: {:halt, index + 1}, else: {:cont, acc}
@@ -32,3 +38,4 @@ end
 
 File.read!("input.txt")
 |> tap(&Day6.part1/1)
+|> tap(&Day6.part2/1)
