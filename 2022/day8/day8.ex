0fe68417633943aad73ecb92d38e0d94a566d75c
gidskql6671 <gidskql6671@gmail.com>
2022-12-13 14:54:05 +0900

    2022 Day8 - Part2 코드 작성

--- 2022/day8/day8.ex
+++ 2022/day8/day8.ex
@@ -6,6 +6,14 @@ defmodule Day8 do
     |> IO.inspect(label: "part1")
   end
 
+  def part2(input) do
+    input
+    |> parse_input()
+    |> calculate_scenic_scores()
+    |> Enum.max()
+    |> IO.inspect(label: "part1")
+  end
+
   defp parse_input(input) do
     input
     |> String.split("\n")
@@ -51,7 +59,81 @@ defmodule Day8 do
       false
     end
   end
+
+  defp calculate_scenic_scores(grid) do
+    n = Enum.count(grid) - 1
+    m = Enum.count(grid[0]) - 1
+
+    for i <- 0..n, j <- 0..m do
+      calculate_scenic_score(grid, i, j, n, m)
+    end
+  end
+
+  defp calculate_scenic_score(grid, y, x, n, m) do
+    get_visible_count(grid, y, x, n, m, :left) *
+      get_visible_count(grid, y, x, n, m, :right) *
+      get_visible_count(grid, y, x, n, m, :up) *
+      get_visible_count(grid, y, x, n, m, :down)
+  end
+
+  defp get_visible_count(_grid, _y, 0, _n, _m, :left), do: 0
+  defp get_visible_count(_grid, _y, x, _n, x, :right), do: 0
+  defp get_visible_count(_grid, 0, _x, _n, _m, :up), do: 0
+  defp get_visible_count(_grid, y, _x, y, _m, :down), do: 0
+
+  defp get_visible_count(grid, y, x, _n, _m, :left) do
+    tree_height = get_in(grid, [y, x])
+
+    Enum.reduce_while((x - 1)..0, 0, fn other_x, acc ->
+      other_height = get_in(grid, [y, other_x])
+
+      cond do
+        other_height >= tree_height -> {:halt, acc + 1}
+        true -> {:cont, acc + 1}
+      end
+    end)
+  end
+
+  defp get_visible_count(grid, y, x, _n, m, :right) do
+    tree_height = get_in(grid, [y, x])
+
+    Enum.reduce_while((x + 1)..m, 0, fn other_x, acc ->
+      other_height = get_in(grid, [y, other_x])
+
+      cond do
+        other_height >= tree_height -> {:halt, acc + 1}
+        true -> {:cont, acc + 1}
+      end
+    end)
+  end
+
+  defp get_visible_count(grid, y, x, _n, _m, :up) do
+    tree_height = get_in(grid, [y, x])
+
+    Enum.reduce_while((y - 1)..0, 0, fn other_y, acc ->
+      other_height = get_in(grid, [other_y, x])
+
+      cond do
+        other_height >= tree_height -> {:halt, acc + 1}
+        true -> {:cont, acc + 1}
+      end
+    end)
+  end
+
+  defp get_visible_count(grid, y, x, n, _m, :down) do
+    tree_height = get_in(grid, [y, x])
+
+    Enum.reduce_while((y + 1)..n, 0, fn other_y, acc ->
+      other_height = get_in(grid, [other_y, x])
+
+      cond do
+        other_height >= tree_height -> {:halt, acc + 1}
+        true -> {:cont, acc + 1}
+      end
+    end)
+  end
 end
 
 File.read!("input.txt")
 |> tap(&Day8.part1/1)
+|> tap(&Day8.part2/1)
