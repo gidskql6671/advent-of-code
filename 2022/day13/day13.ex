e365bd36ec8a60a6f84a51d5610d635bfb5a926c
gidskql6671 <gidskql6671@gmail.com>
2022-12-24 12:55:12 +0900

    2022 Day13 - Part2 코드 작성

--- 2022/day13/day13.ex
+++ 2022/day13/day13.ex
@@ -2,16 +2,27 @@ defmodule Day13 do
   def part1(input) do
     input
     |> parse_input()
+    |> Enum.chunk_every(2)
     |> find_right_order_pair_index()
     |> Enum.sum()
     |> IO.inspect(label: "part1")
+  end
+
+  def part2(input) do
+    divider_packets = [[[2]], [[6]]]
+
+    input
+    |> parse_input()
+    |> Enum.concat(divider_packets)
+    |> Enum.sort(&right_order?/2)
+    |> calculate_decoder_key(divider_packets)
+    |> IO.inspect(label: "part2")
   end
 
   defp parse_input(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&parse_packet/1)
-    |> Enum.chunk_every(2)
   end
 
   defp parse_packet(packet) do
@@ -82,44 +93,53 @@ defmodule Day13 do
   defp find_right_order_pair_index(pairs) do
     pairs
     |> Enum.with_index(1)
-    |> Enum.filter(fn {pair, _index} -> right_order?(pair) end)
+    |> Enum.filter(fn {[left, right], _index} -> right_order?(left, right) end)
     |> Enum.map(&elem(&1, 1))
   end
 
-  defp right_order?([[], []]), do: :cont
-  defp right_order?([[], _right]), do: true
-  defp right_order?([_left, []]), do: false
+  defp right_order?([], []), do: :cont
+  defp right_order?([], _right), do: true
+  defp right_order?(_left, []), do: false
 
-  defp right_order?([left, right]) do
+  defp right_order?(left, right) do
     [l_val | l_remain] = left
     [r_val | r_remain] = right
 
     cond do
       is_integer(l_val) and is_integer(r_val) ->
         if l_val == r_val,
-          do: right_order?([l_remain, r_remain]),
+          do: right_order?(l_remain, r_remain),
           else: l_val < r_val
 
       is_integer(l_val) ->
-        case right_order?([[l_val], r_val]) do
-          :cont -> right_order?([l_remain, r_remain])
+        case right_order?([l_val], r_val) do
+          :cont -> right_order?(l_remain, r_remain)
           result -> result
         end
 
       is_integer(r_val) ->
-        case right_order?([l_val, [r_val]]) do
-          :cont -> right_order?([l_remain, r_remain])
+        case right_order?(l_val, [r_val]) do
+          :cont -> right_order?(l_remain, r_remain)
           result -> result
         end
 
       true ->
-        case right_order?([l_val, r_val]) do
-          :cont -> right_order?([l_remain, r_remain])
+        case right_order?(l_val, r_val) do
+          :cont -> right_order?(l_remain, r_remain)
           result -> result
         end
     end
+  end
+
+  defp calculate_decoder_key(packets, divider_packets) do
+    packets
+    |> Enum.with_index(1)
+    |> Enum.filter(fn {packet, _index} -> Enum.any?(divider_packets, &(&1 == packet)) end)
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.product()
   end
 end
 
 File.read!("input.txt")
 |> tap(&Day13.part1/1)
+|> tap(&Day13.part2/1)
