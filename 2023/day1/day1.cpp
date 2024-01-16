c4cb340aa2ce738bd5b25862f4d8bf2bb3fd5877
gidskql6671 <gidskql6671@gmail.com>
2024-01-16 16:01:16 +0900

    2023 Day1 - Part2 코드 작성

--- 2023/day1/day1.cpp
+++ 2023/day1/day1.cpp
@@ -4,17 +4,9 @@ typedef long long ll;
 using namespace std;
 
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-
-
-  ifstream infile("input.txt");
-
-  string line;
+int part1(vector<string> &input) {
   int result = 0;
-  while (getline(infile, line)) {
+  for(string line : input) {
     int num = 0;
 
     for(int i = 0; i < line.length(); i++) {
@@ -34,7 +26,100 @@ int main() {
     result += num;
   }
 
-  cout << "Part1 : " << result << endl;
+  return result;
+}
+
+int part2(vector<string> input) {
+  string digits[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+  int result = 0;
+  for(string line : input) {
+    int num = 0;
+
+    for(int i = 0; i < line.length(); i++) {
+      if ('0' <= line[i] and line[i] <= '9') {
+        num = 10 * (line[i] - '0');
+        break;
+      }
+
+      bool isNumber = false;
+      for(int j = 0; j < 9; j++) {
+        string digit = digits[j];
+        bool isSame = true;
+        for(int k = 0; k < digit.length() and i + k < line.length(); k++) {
+          if (digit[k] != line[i + k]) {
+            isSame = false;
+            break;
+          }
+        }
+
+        if (isSame) {
+          isNumber = true;
+          num = 10 * (j + 1);
+          break;
+        }
+      }
+
+      if (isNumber) {
+        break;
+      }
+    }
+
+    for(int i = line.length() - 1; i >= 0; i--) {
+      if ('0' <= line[i] and line[i] <= '9') {
+        num += line[i] - '0';
+        break;
+      }
+
+      bool isNumber = false;
+      for(int j = 0; j < 9; j++) {
+        string digit = digits[j];
+        bool isSame = true;
+        if (i - (digit.length() - 1) < 0) {
+          break;
+        }
+
+        for(int k = 0; k < digit.length(); k++) {
+          if (digit[k] != line[i - digit.length() + 1 + k]) {
+            isSame = false;
+            break;
+          }
+        }
+
+        if (isSame) {
+          isNumber = true;
+          num += j + 1;
+          break;
+        }
+      }
+
+      if (isNumber) {
+        break;
+      }
+    }
+
+    result += num;
+  }
+
+  return result;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  ifstream infile("input.txt");
+  vector<string> input;
+
+  string line;
+  while (getline(infile, line)) {
+    input.push_back(line);
+  }
+
+  cout << "Part1 : " << part1(input) << endl;
+  cout << "Part1 : " << part2(input) << endl;
 
   return 0;
 }
